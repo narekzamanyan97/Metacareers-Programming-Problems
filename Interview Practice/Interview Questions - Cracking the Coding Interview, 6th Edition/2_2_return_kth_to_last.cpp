e8c70837abc4dfa59430adcc4764e818214ca897
Narek Zamanyan <narekzamanyan97@gmail.com>
2022-11-06 12:40:08 -0800

    Finished implementing the recursive solution with two return values (wrapped in a par<>).

--- Interview Practice/Interview Questions - Cracking the Coding Interview, 6th Edition/2_2_return_kth_to_last.cpp
+++ Interview Practice/Interview Questions - Cracking the Coding Interview, 6th Edition/2_2_return_kth_to_last.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int return_kth_to_last(LinkedList linked_list, int k);
 
-int return_kth_to_last_recursively(Node* iterator, int& k, int counter);
+int return_kth_to_last_recursively(Node* iterator, int& k, int counter = 0);
 
-// pair<int, int> return_kth_to_last_recursively(Node* iterator, int k);
+pair<int, int> return_kth_to_last_recursively_pair(Node* iterator, int k, int counter = 0);
 
 int main() {
 	LinkedList linked_list = LinkedList(99, 22, 50);
@@ -20,8 +20,8 @@ int main() {
 		cin >> k;
 
 		if(k != -1) {
-			int kth_to_last = return_kth_to_last_recursively(linked_list.begin(), k, 0);
-			cout << kth_to_last << endl;					
+			std::pair<int, int> kth_to_last = return_kth_to_last_recursively_pair(linked_list.begin(), k);
+			cout << kth_to_last.first << ", " << kth_to_last.second << endl;					
 		}
 
 	} while (k != -1);
@@ -89,7 +89,10 @@ int return_kth_to_last(LinkedList linked_list, int k) {
 //		in the process of recursion, there are other values returned as well, that is,
 //			the values of nodes that are ahead of the kth to last node.
 int return_kth_to_last_recursively(Node* iterator, int& k, int counter) {
-	if(iterator != NULL) {
+	if(k < 0) {
+		throw std::out_of_range("Index k cannot be negative.");
+	}
+	else if(iterator != NULL) {
 		// base case: the iterator points to the last element
 		if(iterator->next == NULL){ 
 			// return the value in the last node. 
@@ -106,10 +109,6 @@ int return_kth_to_last_recursively(Node* iterator, int& k, int counter) {
 				// Return the value returned in the previous recursive call 
 				return value;
 			}
-			else if(k < 0) {
-				// a negative index was provided
-				throw std::out_of_range("The index cannot be negative.");
-			}
 			else {
 				// k is still not 0, so the current node is not what we are looking for.
 				//		the node we are looking for is to the left of the current node.
@@ -117,7 +116,7 @@ int return_kth_to_last_recursively(Node* iterator, int& k, int counter) {
 				k--;
 			}
 		}
-		// !!! if k was too large, it never gets to 0, so throw an error
+		// if k was too large, it never gets to 0, so throw an error
 		if(counter == 0 && k > 0) {
 			// we are at the front of the linked list, and k is still > 0, so throw an
 			//		out of range error (k was too large)
@@ -136,50 +135,64 @@ int return_kth_to_last_recursively(Node* iterator, int& k, int counter) {
 }
 
 
-// // solve the problem recursively, returning 2 values from the function enclosed in a 
-// //		pair.
-// //  The first value is the value of k, the second value is the node's value 
-// // @parameters:
-// // 		iterator = a pointer to a node of the list. we start from the beginning (haed) of
-// //			the list
-// //		k = the index (from the end (tail) of the list)
-// // @return:
-// //		the ultimate value returned is !!!
-// pair<int, int> return_kth_to_last_recursively(Node* iterator, int k) {
-// 	if(iterator != NULL) {
-// 		// base case: the iterator points to the last element
-// 		if(iterator->next == NULL){ 
-// 			// return the value in the last node. 
-// 			// This may not be the final return value.
-// 			return std::make_pair(k, iterator->value);
-// 		}
-// 		else {
-// 			// recurse to the next node
-// 			pair<int, int> returned_pair = return_kth_to_last_recursively(iterator->next, k);
+// solve the problem recursively, returning 2 values from the function enclosed in a 
+//		pair.
+//  The first value is the value of k, the second value is the node's value 
+// @parameters:
+// 		iterator = a pointer to a node of the list. we start from the beginning (haed) of
+//			the list
+//		k = the index (from the end (tail) of the list)
+// 		counter = starting from 0, it helps us keep track of the first caller in the 
+//			stack. When it is 0, then it is the first recursive call.
+// @return:
+//		the ultimate value returned is 0 for k and the value of the node that is the kth
+//			to last. if the return value is anything other than 0, then k was out of range
+pair<int, int> return_kth_to_last_recursively_pair(Node* iterator, int k, int counter) {
+	// declare variables for later use
+	int new_k;
+	int value;
 
-// 			// extract the values from the returned pair
-// 			int new_k = returned_pair.first
-// 			int value = returned_pair.second
+	if(k < 0) {
+		throw std::out_of_range("Index k cannot be negative.");
+	}
+	else if(iterator != NULL) {
+		// base case: the iterator points to the last element
+		if(iterator->next == NULL){ 
+			// return the value in the last node. 
+			// This may not be the final return value.
+			return std::make_pair(k, iterator->value);
+		}
+		else {
+			// recurse to the next node
+			pair<int, int> returned_pair = return_kth_to_last_recursively_pair(iterator->next, k, counter + 1);
 
-// 			// we have already reached kth to last element. return its value in a 
-// 			//		preceding recursive call. 
-// 			if(k == 0) {
-// 				// Return the value returned in the previous recursive call 
-// 				return value;
-// 			}
-// 			else {
-// 				// k is still not 0, so the current node is not what we are looking for.
-// 				//		the node we are looking for is to the left of the current node.
-// 				//	decrement k
-// 				k--;
+			// extract the values from the returned pair
+			new_k = returned_pair.first;
+			value = returned_pair.second;
 
-// 				return iterator->value;
-// 			}
-// 		}
-// 	}
-// 	// the list is empty
-// 	else {
-// 		throw std::out_of_range("The linked list is empty.");
-// 	}
-// }
+			// if k is 0, pass the value received from the previous recursive call
+			if(new_k == 0) {
+				return std::make_pair(new_k, value);
+			}
+			else {
+				new_k--;
+			}
+		}
+
+		// if k was too large, it never gets to 0, so throw an error
+		if(counter == 0 && new_k > 0) {
+			// we are at the front of the linked list, and k is still > 0, so throw an
+			//		out of range error (k was too large)
+			throw std::out_of_range("k is larger than the size of the list.");
+		}
+		else {
+			// return the value of the current node
+			return std::make_pair(new_k, iterator->value);
+		}
+	}
+	// the list is empty
+	else {
+		throw std::out_of_range("The linked list is empty.");
+	}
+}
 // Hints: 67, 126
