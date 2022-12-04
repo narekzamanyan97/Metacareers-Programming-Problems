d58bd21e33f31ab0569303673df66c5006749492
Narek Zamanyan <narekzamanyan97@gmail.com>
2022-12-03 17:24:39 -0800

    Finished implementing 2 versions of the recursive is_palindrome.

--- Interview Practice/Interview Questions - Cracking the Coding Interview, 6th Edition/2_6_palindrome.cpp
+++ Interview Practice/Interview Questions - Cracking the Coding Interview, 6th Edition/2_6_palindrome.cpp
@@ -4,11 +4,27 @@
 using namespace std;
 
 
+//////////////////////////////////////////////////////////////////////////////
+//				Approach 1: reverting the string (list) recursively and then
+//					checking for palindrome 
 bool is_palindrome(LinkedList string);
 int revert(LinkedList& string, Node* iterator);
 
+//////////////////////////////////////////////////////////////////////////////
+//				Approach 2: Assuming length() of string is known. Recursive
+//					using hint 29
+pair<bool, Node*> is_palindrome(Node* left_it, Node* right_it, int& counter, int length);
+
+//////////////////////////////////////////////////////////////////////////////
+//				Approach 3: Assuming length() of string is known. Recursive
+//					using hint 61
+pair<bool, Node*> is_palindrome(Node* left_it, int& counter, int length);
+
+
 int main() {
 	int seed;
+
+	int counter = 0;
 
 	do {
 		cout << "Enter any digit to continue. Enter -1 to exit: " << endl;
@@ -18,25 +34,31 @@ int main() {
 			// //////////////////////////////////////////////////////////////////
 			//					Testing is_palindrome(LinkedList string)
 			LinkedList string = LinkedList();
-			string.add_last(9);
-			string.add_last(1);
-			string.add_last(4);
-			string.add_last(2);
-			string.add_last(7);
-			string.add_last(0);
-			string.add_last(1);
+			// string.add_last(9);
+			// string.add_last(1);
+			// string.add_last(4);
+			// string.add_last(2);
+			// string.add_last(7);
+			// string.add_last(0);
+			// string.add_last(9);
 
-			string.add_last(8);
+			// string.add_last(8);
 
-			string.add_last(1);
-			string.add_last(0);
-			string.add_last(7);
-			string.add_last(2);
-			string.add_last(4);
-			string.add_last(1);
-			string.add_last(9);
+			// string.add_last(9);
+			// string.add_last(0);
+			// string.add_last(7);
+			// string.add_last(2);
+			// string.add_last(4);
+			// string.add_last(1);
+			// string.add_last(9);
 
-			if(is_palindrome(string)) {
+			// reset the counter to 0 for the next call
+			counter = 0;
+
+			string.print();
+
+			pair<bool, Node*> is_pal = is_palindrome(string.begin(), counter, string.length());
+			if(is_pal.first) {
 				cout << "palindrome" << endl;
 			}		
 			else {
@@ -114,7 +136,7 @@ bool is_palindrome(LinkedList string) {
 // @return:
 //		the length of the linked list
 int revert(LinkedList& string, Node* iterator) {
-	if(iterator != NULL) {
+	if(iterator != nullptr) {
 		// recurse to the next node
 		int length = revert(string, iterator->next);
 
@@ -133,3 +155,149 @@ int revert(LinkedList& string, Node* iterator) {
 	}
 }
 
+
+//////////////////////////////////////////////////////////////////////////////
+//				Approach 2: Assuming length() of string is known. Recursive
+//					using hint #29
+// the base case is the last node
+// @parameters:
+//		left_it = the head of the list. We keep this the same throughout the
+//			recursion. When it is time, we move to the left
+//		right_it = the iterator in the right half. starts from the right end 
+//			(tail) of the list
+//		counter = keeps track of the number of opposite nodes being compared. 
+// 		lenght = the length of the list. cannot be changed
+// @return:
+//		a pair of bool and iterator
+//			pair.first:		
+//				true if the list is a palindrome
+//				false otherwise
+//		iterator
+//			a pointer to the left iterator for the current check
+pair<bool, Node*> is_palindrome(Node* left_it, Node* right_it, int& counter, int length) {
+	// move the right_it to the right until the end (tail) is reached
+	if(right_it != nullptr) {
+		// recurse, moving the right iterator to the next node
+		pair<bool, Node*> is_pal_and_left = is_palindrome(left_it, right_it->next, counter, length);
+
+		// declare current left node
+		Node* current_left_it = is_pal_and_left.second;
+		
+		// if the previous recursive call returned false, just return false.
+		//		otherwise, if counter < length/2, continue the check
+		if(is_pal_and_left.first && counter < length/2) {
+			
+			// at this point, the right_it and current_left_it are the same length
+			// 		away from the mid point of the string. compare
+			if(current_left_it->value == right_it->value) {
+				cout << current_left_it->value << " == " << right_it->value << endl;
+				// increment the current_left_it
+				current_left_it = current_left_it->next; 			
+
+				// increment the counter
+				counter++;
+
+				return std::make_pair(true, current_left_it);				
+			}
+			else {
+				cout << current_left_it->value << " != " << right_it->value << endl;
+				// return false
+				return std::make_pair(false, left_it);
+			}
+		}
+		// either reached the middle element or palindrome check failed at some
+		// 		point
+		else {
+			// the check has not failed at some point.
+			if(is_pal_and_left.first) {
+				// reached the middle element (if there is one), return true
+				return std::make_pair(true, left_it);				
+			}
+			// a check has failed.
+			else {
+				// if false was returned in the recursive call, then we must return
+				//		false
+				return std::make_pair(false, left_it);				
+			}
+		}
+
+
+	}
+	// base case, the right_it has reached the tail of the list. return true
+	//		to continue the process
+	else {
+		return std::make_pair(true, left_it);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//				Approach 3: Assuming length() of string is known. Recursive
+//					using hint 61
+// the base case is the middle node
+// @parameters:
+//		left_it = a pointer to the node that keeps track of the left iterator
+//		counter = keeps track of the number of opposite nodes being compared. 
+// 		lenght = the length of the list. cannot be changed
+// @return:
+//		a pair of bool and iterator
+//			pair.first:		
+//				true if the list is a palindrome
+//				false otherwise
+//		iterator
+//			a pointer to the iterator on the right half of the linked list. The
+//				symmetric pair of the left_it
+pair<bool, Node*> is_palindrome(Node* left_it, int& counter, int length){
+	// proceed only when the string is not empty
+	if(length != 0) {
+		// base case 1: counter => length / 2
+		if(counter == length / 2) {
+			// 		(a) if length is even, return the left_it as the iterator in the 
+			//			right half
+			if(length % 2 == 0) {
+				return std::make_pair(true, left_it);
+			}
+			//  	(b) if it's odd (there is a single middle node), return the next of
+			//			left_it
+			//		as the iterator in the right half.
+			else {
+				return std::make_pair(true, left_it->next);
+			}		
+		}
+		else {
+			// recurse
+			pair<bool, Node*> is_pal_and_right = is_palindrome(left_it->next, ++counter, length); 
+			
+			// extract the values from the pair returned from the recursive call
+			bool is_pal_so_far = is_pal_and_right.first;
+			Node* right_it = is_pal_and_right.second;
+
+			// so far so good. Continue comparing symmetric nodes
+			if(is_pal_so_far) {
+				// compare the values of symmetric nodes (right and left iterators)
+				if(left_it->value == right_it->value) {
+					cout << left_it->value << " == " << right_it->value << endl;
+					// return the right node of the right iterator and true
+					return std::make_pair(true, right_it->next);
+				}
+				// values are different, so not a palindrome
+				else {
+					cout << left_it->value << " != " << right_it->value << endl;
+					// the second value of the pair does not matter
+					return std::make_pair(false, right_it);
+				}
+			}
+			// if the palindrome has failed, retusrn false
+			else {
+				// no need to compare, just return falsed
+				return std::make_pair(false, right_it);
+			}
+		}	
+
+	}
+	else {
+		// the string (linked list) is empty, so return true
+		return std::make_pair(true, left_it);
+	}
+}
+
+// hints: 5, 13, 29, 61, 101
---
+++ Interview Practice/Interview Questions - Cracking the Coding Interview, 6th Edition/2_7_intersection.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../../Data Structures - Implementations/Linked List (Double)/LinkedList.cpp"
+#include <ctime>
+using namespace std;
+
+
+int main() {
+
+	return 0;
+}
